1aa6dc8e49d1a1eaf7af4c5a6251b5bd69b8bad8
ishasingh11 <isha11.dav@gmail.com>
2019-10-09 17:13:39 +0530

    Add 2 numbers

---
+++ Add2Num.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Add2Num {
+
+	public static void main(String[] args) {
+		Scanner scan=new Scanner(System.in);
+		int i=scan.nextInt();
+		int j=scan.nextInt();
+		int k=i+j;
+		System.out.println(k);
+	}
+
+}
