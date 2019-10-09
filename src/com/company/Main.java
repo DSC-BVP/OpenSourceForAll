b74c07fd8a9c778cb83a2a78720e523e5e04c3b3
sjain30 <43096728+sjain30@users.noreply.github.com>
2019-10-09 17:33:40 +0530

    MyContribution

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("I love Open Source because it is the future");
+        System.out.println("GitHub rocks");
     }
 }
