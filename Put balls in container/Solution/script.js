fa0c3b3ef0f40cea36f3e944983a5b8d1381d094
ritikadhawan <ritikadhawan28@gmail.com>
2019-10-16 23:39:07 +0530

    extra files deleted

--- Put balls in container/Solution/script.js
+++
@@ -1,8 +0,0 @@
-console.log("hello world");
-var colors = ['red', 'blue', 'yellow', 'lightgrey', 'darkorchid', 'black', 'orange', 'deeppink', 'green', 'purple', 'saddlebrown', 'lightseagreen', 'deepskyblue', 'firebrick', 'crimson'];
-
-var box = $("#box");
-
-$("button").click(function(e) {
-    box.append('<div class="ball" style="background-color: ' + colors[Math.floor(Math.random()*15)] + '"></div>');
-});
