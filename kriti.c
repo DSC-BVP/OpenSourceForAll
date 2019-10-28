f5ee57e8b63d41159895b9a5133e2a37181a3edb
Kritika <kritika.dewan11@gmail.com>
2019-10-28 14:33:20 +0530

    added my opinion

---
+++ kriti.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+void main()
+{ printf("I love Open source because anyone can contribute in it");
+printf("it motivates me to pay more attention on software designing and testing");
+getch();
+}
